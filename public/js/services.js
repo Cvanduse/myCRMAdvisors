c8d9fc10f204c056724dae3d806af36f1288c4d2
Christopher Van Dusen <Cvanduse@gmail.com>
2025-04-06 12:00:45 -0400

    optimize

--- public/js/services.js
+++ public/js/services.js
@@ -1,39 +1,27 @@
 (() => {
   // <stdin>
-  document.addEventListener("DOMContentLoaded", function() {
-    const serviceList = document.querySelector(".service-list ul");
-    const serviceListItems = document.querySelectorAll(".service-list-item");
-    const serviceDetailContents = document.querySelectorAll(".service-detail-content");
-    if (!serviceList || !serviceListItems.length || !serviceDetailContents.length) {
-      return;
-    }
-    function activateService(serviceId) {
-      document.querySelector(".service-list-item.active")?.classList.remove("active");
-      document.querySelector(".service-detail-content.active")?.classList.add("d-none");
-      document.querySelector(".service-detail-content.active")?.classList.remove("active");
-      const listItem = document.querySelector(`.service-list-item[data-service-id="${serviceId}"]`);
-      const detailContent = document.getElementById(serviceId);
-      if (listItem && detailContent) {
-        listItem.classList.add("active");
-        detailContent.classList.remove("d-none");
-        detailContent.classList.add("active");
-      }
-    }
-    serviceListItems.forEach((item) => {
-      item.addEventListener("mouseover", function() {
-        serviceList.classList.add("list-active");
-        const serviceId = this.getAttribute("data-service-id");
-        activateService(serviceId);
+  document.addEventListener("DOMContentLoaded", () => {
+    const e = document.querySelector(".service-list ul"), t = document.querySelectorAll(".service-list-item"), i = document.querySelectorAll(".service-detail-content");
+    if (e && t.length && i.length) {
+      let s2 = function(s3) {
+        document.querySelector(".service-list-item.active")?.classList.remove("active"), document.querySelector(".service-detail-content.active")?.classList.add("d-none"), document.querySelector(".service-detail-content.active")?.classList.remove("active");
+        const c = document.querySelector(`.service-list-item[data-service-id="${s3}"]`), n = document.getElementById(s3);
+        c && n && (c.classList.add("active"), n.classList.remove("d-none"), n.classList.add("active"));
+      };
+      var s = s2;
+      t.forEach((t2) => {
+        t2.addEventListener("mouseover", function() {
+          e.classList.add("list-active");
+          const t3 = this.getAttribute("data-service-id");
+          s2(t3);
+        }), t2.addEventListener("touchstart", function(t3) {
+          t3.preventDefault(), e.classList.add("list-active");
+          const i2 = this.getAttribute("data-service-id");
+          s2(i2);
+        });
+      }), e.addEventListener("mouseleave", function() {
+        e.classList.remove("list-active");
       });
-      item.addEventListener("touchstart", function(event) {
-        event.preventDefault();
-        serviceList.classList.add("list-active");
-        const serviceId = this.getAttribute("data-service-id");
-        activateService(serviceId);
-      });
-    });
-    serviceList.addEventListener("mouseleave", function() {
-      serviceList.classList.remove("list-active");
-    });
+    }
   });
 })();
